99158a35fe9d762ae86226fafd8f79882bf2545c
wjt <1109765190@qq.com>
2020-11-18 23:46:40 +0800

    创建了前端vue基础

--- vuevideoanalysis/src/App.vue
+++ vuevideoanalysis/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+    <!-- <router-view /> -->
   </div>
 </template>
 
@@ -15,8 +16,8 @@ import axios from "axios";
 
 export default {
   name: "app",
-  
-  created: function () { 
+
+  created: function () {
     //窗体加载时执行
     this.get();
   },
@@ -41,24 +42,58 @@ export default {
 </script>
 
 <style>
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+  height: 100%;
 }
 
-#nav {
-  padding: 30px;
+.el-container.is-vertical {
+    flex-direction: column;
+    height: 100%;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
 }
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+  height: 60%;
+}
+
+/* body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+} */
 </style>

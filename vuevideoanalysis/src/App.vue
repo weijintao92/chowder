a8e453b993c270cf4501a89bc7fafb6e6a198acf
wjt <1109765190@qq.com>
2020-11-18 23:22:51 +0800

    创建了前端vue基础

---
+++ vuevideoanalysis/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+
+export default {
+  name: "app",
+  
+  created: function () { 
+    //窗体加载时执行
+    this.get();
+  },
+  methods: {
+    get() {
+      axios
+        .get("http://180.76.98.78/snippets/?format=json")
+        .then(function (response) {
+          // handle success
+          console.log(response);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .then(function () {
+          // always executed
+        });
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>

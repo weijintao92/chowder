75feee3468925016652cdcc9146e214c047f47a8
wjt <1109765190@qq.com>
2020-11-19 20:53:00 +0800

    新增vue 解析页面

--- vuevideoanalysis/src/views/Home.vue
+++ vuevideoanalysis/src/views/Home.vue
@@ -2,14 +2,16 @@
   <!-- <div id="app"> -->
   <el-container>
     <el-header
-      ><h1>格调vip解析</h1>
-      <h4>
-        免责声明：本站所有视频资源均来自网络。服务器仅展示第三方网站接口页面，并不存储任何视频资源。因此经由本站搜索所产生
-        的任何结果皆不代表本站立场，本站不对其真实合法性以及版权负责，亦不承担任何法律责任。
-      </h4></el-header
+      >
+      <h1>格调vip解析</h1>
+      <h4> 免责声明：本站所有视频资源均来自网络。服务器仅展示第三方网站接口页面，并不存储任何视频资源。因此经由本站搜索所产生
+        的任何结果皆不代表本站立场，本站不对其真实合法性以及版权负责，亦不承担任何法律责任。</h4>
+    
+        
+      </el-header
     >
     <el-main>
-      <el-row>
+      <el-row style="  margin-top: -12px;" >
         <el-col :span="4">
           <el-select v-model="my_analysis" placeholder="请选择">
             <el-option
@@ -30,13 +32,13 @@
         </el-col>
       </el-row>
 
-      <div style="margin-top: 5px; border: 1px solid yellow">
+      <el-row >
         <el-button type="primary" @click="begin_analysis">Go-开始解析</el-button
         ><el-button type="primary" @click="begin_analysis_full"
           >Go-全屏解析</el-button
-        >
-      </div>
-      <div style="border: 1px solid red; margin-top: 2px;height100%">
+        ></el-row
+      >
+      <el-row style="height:500px" >
         <iframe
           id="google_esf"
           name=" google_esf"
@@ -50,7 +52,7 @@
           allowfullscreen="true"
           allowtransparency="true"
         ></iframe>
-      </div>
+      </el-row>
     </el-main>
     <el-footer>
       <h4>
@@ -135,26 +137,32 @@ export default {
 };
 </script>
 <style>
-.el-container{
-position: absolute;
-height: 98.5%;
-width: 99%;
+.el-container {
+  display: flex;
+  justify-content: center;
+  height: auto;
+  width: 99%;
 }
 .el-col {
   margin-left: -5px !important;
+}
+.el-row{
+  margin-top: 5px;
 }
 .el-header,
 .el-footer {
   background-color: #b3c0d1;
   color: #333;
   text-align: center;
-  height: 20% !important;
+  height: auto !important;
+
 }
 
 .el-main {
   background-color: #e9eef3;
   color: #333;
   text-align: center;
-  height: 60% !important;
+    height: auto !important;
+ 
 }
 </style>

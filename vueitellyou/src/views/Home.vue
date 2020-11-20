a0a81fa78987c909d063c75e07e9fb91ca5493af
1109765190@qq.com <1109765190@qq.com>
2020-11-20 18:00:41 +0800

    i tell you

---
+++ vueitellyou/src/views/Home.vue
@@ -0,0 +1,176 @@
+<template>
+  <el-container>
+    <el-header>
+      <div>
+        <el-image
+          style="height: 44px; width: 136px"
+          :src="url"
+          :fit="fit"
+        ></el-image>
+      </div>
+      <div class="my_div">
+        <div
+          style="padding: 0 10px"
+          v-for="(item, index) in head_buttons"
+          :key="index"
+        >
+          <el-button type="text">{{ item.name }}</el-button>
+        </div>
+        <el-dropdown>
+          <span class="el-dropdown-link">
+            联系我<i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>黄金糕</el-dropdown-item>
+            <el-dropdown-item>狮子头</el-dropdown-item>
+            <el-dropdown-item>螺蛳粉</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </el-header>
+    <el-container style="width: 80%">
+      <el-aside width="200px"
+        ><el-row class="tac">
+
+          <el-col :span="12">
+            <h5>自定义颜色</h5>
+            <el-menu
+              default-active="2"
+              class="el-menu-vertical-demo"
+              @open="handleOpen"
+              @close="handleClose"
+              background-color="#545c64"
+              text-color="#fff"
+              active-text-color="#ffd04b"
+            >
+              <el-submenu index="1">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>导航一</span>
+                </template>
+                <el-menu-item-group>
+                  <template slot="title">分组一</template>
+                  <el-menu-item index="1-1">选项1</el-menu-item>
+                  <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                  <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                  <template slot="title">选项4</template>
+                  <el-menu-item index="1-4-1">选项1</el-menu-item>
+                </el-submenu>
+              </el-submenu>
+              <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+              </el-menu-item>
+              <el-menu-item index="3" disabled>
+                <i class="el-icon-document"></i>
+                <span slot="title">导航三</span>
+              </el-menu-item>
+              <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航四</span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row></el-aside
+      >
+      <el-container>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      fits: ["fill", "contain", "cover", "none", "scale-down"],
+      url: "https://msdn.itellyou.cn/images/itellyou.cn.png",
+      head_buttons: [
+        { name: "站长备用", url: "", my_icon: "" },
+        { name: "十年相伴", url: "", my_icon: "" },
+        { name: "最新更新", url: "", my_icon: "" },
+      ],
+    };
+  },
+      methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+};
+</script>
+
+
+<style>
+.el-dropdown-link {
+  cursor: pointer;
+  color: #409eff;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+.my_div {
+  display: flex;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-container.is-vertical {
+  flex-direction: column;
+  align-items: center;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+.el-image {
+  position: relative;
+  display: block;
+  overflow: hidden;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+.el-button:hover {
+  color: red;
+}
+</style>

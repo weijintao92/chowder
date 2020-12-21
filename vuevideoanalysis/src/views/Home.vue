365a1c74c655507b9ac0095cfb190808665f7977
weijintao92 <47779463+weijintao92@users.noreply.github.com>
2020-12-21 17:29:20 +0800

    1
    1

--- vuevideoanalysis/src/views/Home.vue
+++ vuevideoanalysis/src/views/Home.vue
@@ -90,7 +90,7 @@ export default {
     //窗体加载，基础的加载
     my_window_load() {
       axios
-        .get("http://api.gd97.xyz/GetVideoUrls/")
+        .get("127.0.0.1:8000/GetVideoUrls/")
         .then((response) => {
           // handle success
           // console.log(response);

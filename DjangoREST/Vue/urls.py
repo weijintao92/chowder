eae24061729664dddeca4aa4dbf5fb36b7f1f937
weijintao92 <47779463+weijintao92@users.noreply.github.com>
2021-01-08 15:24:29 +0800

    1
    1

--- DjangoREST/Vue/urls.py
+++ DjangoREST/Vue/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from Vue import views
 
 urlpatterns = [
-    url(r'^get_gitHub_accessToken/$', views.get_gitHub_accessToken),
+    url(r'^get_gitHub_accessToken/$',views.get_gitHub_accessToken)
 ]
--- DjangoREST/Vue/views.py
+++ DjangoREST/Vue/views.py
@@ -1,46 +1,62 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import requests
 
-# Create your views here.
-
 @api_view(['POST'])
 def get_gitHub_accessToken(request):
-    """获取gitHub的AccessToken
+    """获取AccessToken.
 
-    向https://github.com/login/oauth/access_token发起POST请求，传递3个参数client_id、client_secret、code
-    得到gitHub的AccessToken
+    Args:
+        code: .
+        client_id: 
+        client_secret: 
 
-    Args:
-        client_id: 
-        client_secret: 
-        code: 
+    Returns:
+        用户信息
 
-    Returns:
-        AccessToken
-
-    Raises:
+    Raises:
+        
     """
     if request.method == 'POST':
         # 获取传递的参数
         code = request.POST.get("code")
-        my_type = request.POST.get("my_type")
+        client_id = request.POST.get("client_id")
+        client_secret = request.POST.get("client_secret")
         # 组装参数
-        if my_type==='dev':
-            payload  = '{"client_id":"5eeb1fbb5d00630d81d0",
-        "client_secret":"56aa7da20ef8fb37fe33989ea0477d89a275ad09",
-        "code":code}'
-        else:
-            payload  = '{"client_id":"dfa7e273d6764be24415",
-        "client_secret":"86c6a06bf205a4058f6684173bfff945d4c1a71a",
-        "code":code}'
-        
-
+        payload = {"client_id": client_id,
+                    "client_secret": client_secret,
+                    "code": code}
         url = "https://github.com/login/oauth/access_token"
-        res = requests.post(url=url,data=payload)
-        return Response(data=res.text)
+        res = requests.post(url=url, data=payload)
+        list_re = res.text.split('&')
+        if list_re[0] == 'error=bad_verification_code' :
+            return Response(data="code过期了！", status=404)
+        access_token = list_re[0].split('=')[1]
+        return Response(data=access_token, status=200)
+    else:
+        return Response(data="错误的请求类型！", status=404)
 
+def get_gitHub_user(access_token):
+    """获取gitHub用户信息
+    
+    Args:
+        access_token
+    
+    Returns:
+
+    Raises:
+    """
+    headers = {
+            'Authorization': 'Basic {}'.format(get_authorization()),
+        }
+    payload = {"client_id": client_id,
+                    "client_secret": client_secret,
+                    "code": code}
+    try:
+        res = requests.get(url='https://api.github.com/user?'+access_token)
+        return Response(data=res.text,status=200)
+    except Exception:
+        return Response(data=Exception,status=401)
 
 
 

dea4f1b7797df85794cbda9c862b3369c4465e0b
weijintao92 <47779463+weijintao92@users.noreply.github.com>
2021-01-12 14:00:30 +0800

    2
    2

--- DjangoREST/Vue/views.py
+++ DjangoREST/Vue/views.py
@@ -30,33 +30,13 @@ def get_gitHub_accessToken(request):
         res = requests.post(url=url, data=payload)
         list_re = res.text.split('&')
         if list_re[0] == 'error=bad_verification_code' :
-            return Response(data="code过期了！", status=404)
+            return Response(data="code过期了！", status=202)
         access_token = list_re[0].split('=')[1]
         return Response(data=access_token, status=200)
     else:
-        return Response(data="错误的请求类型！", status=404)
+        return Response(data="请使用post发起请求！", status=202)
 
-def get_gitHub_user(access_token):
-    """获取gitHub用户信息
-    
-    Args:
-        access_token
-    
-    Returns:
 
-    Raises:
-    """
-    headers = {
-            'Authorization': 'Basic {}'.format(get_authorization()),
-        }
-    payload = {"client_id": client_id,
-                    "client_secret": client_secret,
-                    "code": code}
-    try:
-        res = requests.get(url='https://api.github.com/user?'+access_token)
-        return Response(data=res.text,status=200)
-    except Exception:
-        return Response(data=Exception,status=401)
 
 
 
